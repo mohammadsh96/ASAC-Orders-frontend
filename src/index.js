1455eb0b5c1fc0bbfa38f89045dd2a87fddb66fc
mohammadsh96 <mhmd.shrydh1996@gmail.com>
2023-06-11 08:16:03 +0000

    installing the app

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// src/index.js
 
+import * as serviceWorker from './service-worker';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'my-app-cache';
+const urlsToCache = ['/', '/index.html', '/css/style.css', '/js/app.js'];
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = ['my-app-cache'];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/serviceWorker.js')
+        .then((registration) => {
+          console.log('Service worker registered:', registration);
+        })
+        .catch((error) => {
+          console.log('Service worker registration failed:', error);
+        });
+    });
+  }
+}
