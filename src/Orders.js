14b7af336a0e81a0476197e67cf2261704a4fd19
mohammadsh96 <mhmd.shrydh1996@gmail.com>
2023-07-06 07:27:32 +0000

    add shark menu

--- src/Orders.js
+++ src/Orders.js
@@ -252,7 +252,7 @@ const Orders = () => {
     // Calculate unit price for dividing among orders
     const incrementValues = [0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1.00, 2.00, 2.00, 2.00];
 
-    let incrementIndex = (2 / totalNumOrders)
+    let incrementIndex = (2.5 / totalNumOrders)
 
     for (let i = 0; i < incrementValues.length; i++) {
 
